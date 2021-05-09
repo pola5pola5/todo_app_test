e88709051f202e1565f5b8ffae2d08c6c540a894
pola5pola5 <l.c.a.m.ayn@gmail.com>
2021-05-09 16:17:01 +0900

    create GOTODO

--- example.sql
+++ example.sql
@@ -4,24 +4,24 @@ drop table todos;
 
 create table users (
     id         serial primary key,
-    uuid       varcher(64) not null unique,
-    name       varcher(255),
-    email      varcher(255) not null unique,
-    password   varcher(255) not null,
+    uuid       varchar(64) not null unique,
+    name       varchar(255),
+    email      varchar(255) not null unique,
+    password   varchar(255) not null,
     created_at timestamp not null
 );
 
 create table sessions (
     id         serial primary key,
-    uuid       varcher(64) not null unique,
-    name       varcher(255),
+    uuid       varchar(64) not null unique,
+    name       varchar(255),
     user_id    integer references users(id),
     created_at timestamp not null
 );
 
 create table todos (
     id         serial primary key,
-    uuid       varcher(64) not null unique,
+    uuid       varchar(64) not null unique,
     content    text,
     user_id    integer references users(id),
     created_at timestamp not null
